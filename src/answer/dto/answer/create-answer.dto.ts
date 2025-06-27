300207569ec6a5b0a31b75cf0468cb3fdbaeef32
silofn523 <silofn523@gmail.com>
2025-06-27 19:51:16 +0900

    test

--- src/answer/dto/answer/create-answer.dto.ts
+++ src/answer/dto/answer/create-answer.dto.ts
@@ -1,7 +1,12 @@
 // create-answer.dto.ts
+import { ApiProperty } from '@nestjs/swagger'
 import { IsString } from 'class-validator'
 
 export class CreateAnswerDto {
   @IsString()
+  @ApiProperty({
+    description: '사용자가 입력한 텍스트',
+    default: '잘 모르겠습니다.'
+  })
   public userResponse!: string
 }
